b890086298bc5cb1e3530cf00c13a0b158007ca5
fakerr-sketch <fm3209742@gmail.com>
2023-01-11 13:24:36 -0300

    debug:add

--- cli.js
+++ cli.js
@@ -1,15 +1,18 @@
-const { program } = require('commander');
+const { program, Argument } = require('commander');
+const { _log } = require('./src/utils.js');
 const { version, name } = require('./package.json');
 const deploy = require('./src/index.js');
 
 program
   .name(name)
   .description('Utility to deploy discord slash commands easily.')
-  .version(`${name}, version ${version}.`);
+  .version(`${name}@${version}.`);
+program.option('-d, --debug', 'run in debug mode.');
 program.showSuggestionAfterError(true);
 program.addHelpCommand('--help', 'Displays CLI help.');
 program
   .command('scan')
-  .description('Scan and deploy files.')
+  .alias('s')
+  .description('Scan and deploy files with debug mode (defaults to false). ')
   .action(deploy);
-program.parse();
+program.parse(process.argv);
