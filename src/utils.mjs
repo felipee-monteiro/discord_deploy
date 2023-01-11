121a495d010e07bbcb7d76caae7cb51a5d1b3722
fakerr-sketch <fm3209742@gmail.com>
2023-01-11 18:43:27 -0300

    code:refactoring

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,6 +1,25 @@
-const debugMode = process.argv.some(arg => arg === '--debug');
+import { SlashCommandBuilder } from 'discord.js';
+import colors from 'ansi-styles';
+
+var debugMode = process.argv.some(arg => arg === '--debug');
+
 export const _log = function (message) {
   if (debugMode) {
-    console.info(`[DEBUG] ${message}`);
+    console.info(
+      `${colors.cyanBright.open}[DEBUG] ${message}${colors.cyanBright.close}`
+    );
+  }
+};
+export const validateCommandObject = function (obj) {
+  for (let key in obj) {
+    if (
+      Object.hasOwnProperty.call(obj, key) &&
+      Object.hasOwnProperty.call(obj[key], 'data') &&
+      obj[key].data instanceof SlashCommandBuilder
+    ) {
+      var file = obj[key]['data'];
+      _log(file['name']);
+      return file.toJSON();
+    }
   }
 };
