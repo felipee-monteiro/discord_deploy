3589863340eb9801bccd7609a3f5249437cbf91a
fakerr-sketch <fm3209742@gmail.com>
2023-01-12 10:09:50 -0300

    commandsData:bug:resolve

--- src/index.mjs
+++ src/index.mjs
@@ -1,32 +1,43 @@
-import path from 'node:path';
+import { dirname } from 'node:path';
 import nodeevents from 'node:events';
 import { fileURLToPath } from 'node:url';
-import { validateCommandObject, _log } from './utils.mjs';
+import { _log } from './utils.mjs';
 import glob from 'glob';
-import forEach from 'lodash.foreach';
 import { config } from 'dotenv';
-import { REST, Routes } from 'discord.js';
+import { REST, Routes, SlashCommandBuilder } from 'discord.js';
 
 var __filename = fileURLToPath(import.meta.url);
-var __dirname = path.dirname(__filename);
+var __dirname = dirname(__filename);
 
 nodeevents.setMaxListeners(150);
 config();
 
 var api = new REST({ version: '10' }).setToken(process.env['TOKEN']);
+var commandsData = [];
+
+function validateCommandObject (obj) {
+  for (const key in obj) {
+    if (
+      Object.hasOwnProperty.call(obj[key], 'data') &&
+      obj[key].data instanceof SlashCommandBuilder
+    ) {
+      var file = obj[key].data;
+      _log(file['name']);
+      commandsData.push(file.toJSON());
+    }
+  }
+}
 
 async function getCommandFiles () {
-  var commandsData = [];
   var files = glob.sync('../commands/**/*.js', { cwd: __dirname });
   for (let file of files) {
-    commandsData.push(validateCommandObject(await import(file)));
+    validateCommandObject(await import(file));
   }
-  return commandsData.filter(cmd => typeof cmd !== 'undefined');
 }
 
 async function deploy () {
-  const commandsData = await getCommandFiles();
   try {
+    await getCommandFiles();
     const resolved = await api.put(
       Routes.applicationGuildCommands(
         process.env['CLIENT_ID'],
@@ -38,10 +49,10 @@ async function deploy () {
     );
     resolved.length
       ? _log('Deployed Success!')
-      : _log('Error while tryng deploy files.');
+      : _log('Error while tryng deploy files.', 'error');
     process.exit(0);
   } catch (e) {
-    throw new Error(e);
+    _log(e, 'error');
   }
 }
 export default deploy;
