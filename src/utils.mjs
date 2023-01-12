ea42b9ebc5380f5e7c769de39c5f3bf3d23b454a
fakerr-sketch <fm3209742@gmail.com>
2023-01-12 09:52:24 -0300

    log-levels:add

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,23 +1,43 @@
 import { SlashCommandBuilder } from 'discord.js';
 import colors from 'ansi-styles';
 
-var debugMode = process.argv.some(arg => arg === '--debug');
+var _logLevels = [
+  {
+    level: 'info',
+    color: 'cyanBright',
+    prefix: '[INFO]'
+  },
+  {
+    level: 'warn',
+    color: 'yellow',
+    prefix: '[WARNING]'
+  },
+  {
+    level: 'error',
+    color: 'redBright',
+    prefix: '[ERROR]'
+  }
+];
 
-export const _log = function (message) {
-  if (debugMode) {
-    console.info(
-      `${colors.cyanBright.open}[DEBUG] ${message}${colors.cyanBright.close}`
+var debugMode = process.argv.some(arg => arg === '--debug');
+export const _log = function (message, level = 'info') {
+  var _logProps = _logLevels.find(log => log.level === level);
+  if (debugMode && _logProps) {
+    console[_logProps.level](
+      colors[_logProps.color]['open'],
+      _logProps.prefix,
+      message,
+      colors[_logProps.color]['close']
     );
   }
 };
 export const validateCommandObject = function (obj) {
-  for (let key in obj) {
+  for (const key in obj) {
     if (
-      Object.hasOwnProperty.call(obj, key) &&
       Object.hasOwnProperty.call(obj[key], 'data') &&
       obj[key].data instanceof SlashCommandBuilder
     ) {
-      var file = obj[key]['data'];
+      var file = obj[key].data;
       _log(file['name']);
       return file.toJSON();
     }
