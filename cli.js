868cc093a0f7ccec8d113332fe15613e1b16bd4b
fakerr-sketch <fm3209742@gmail.com>
2023-01-14 16:21:10 -0300

    feature:test:add

---
+++ cli.js
@@ -0,0 +1,24 @@
+import { program, createCommand, Argument } from 'commander';
+import deploy from './src/index.js';
+import * as cli from './package.json' assert { type: 'json' };
+
+program
+  .name(cli.default.name)
+  .description(cli.default.description)
+  .version(`${cli.default.name}@${cli.default.version}.`);
+program.addHelpCommand('--help', 'CLI help.');
+program.showSuggestionAfterError(true);
+program
+  .command('deploy')
+  .alias('d')
+  .option('-d, --debug', 'run in debug mode.', false)
+  .option('--cwd <dir>', 'Absolute directory to searches for.', process.cwd())
+  .option(
+    '--test',
+    'Enables test mode (Requires GUILD_TEST_ID env key). defaults to false.',
+    false
+  )
+  .description('Scan and deploy files.')
+  .action(deploy);
+
+program.parse();
