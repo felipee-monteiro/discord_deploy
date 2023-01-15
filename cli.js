4773dc11a978e8d54b05f5c40b4269f2550b7f3c
fakerr-sketch <fm3209742@gmail.com>
2023-01-15 17:23:59 -0300

    notifier:add

--- cli.js
+++ cli.js
@@ -1,15 +1,22 @@
-import deploy from './src/index.js';
+import notifier from 'update-notifier';
 import { program, Command } from 'commander';
+import deploy from './src/index.js';
 import * as cli from './package.json' assert { type: 'json' };
 
-const {
-  default: { name, version, description }
-} = cli;
+const { name, version, description } = cli.default;
+
+notifier({
+  pkg: {
+    name,
+    version
+  },
+  updateCheckInterval: 0
+}).notify();
 
 program
   .name(name)
   .version(version)
-  .showSuggestionAfterError(false)  
+  .showSuggestionAfterError(false)
   .addCommand(
     new Command('deploy')
       .option('-d, --debug', 'run in debug mode.', false)
@@ -23,7 +30,8 @@ program
         'Enables test mode (Requires GUILD_TEST_ID env key).',
         false
       )
-      .description('Deploy your commands files by searching for "commands" directory in your project. (ATTENTION: Needs "TOKEN" variable in .env)')
+      .description(
+        'Deploy your commands files by searching for "commands" directory in your project. (ATTENTION: Needs "TOKEN" variable in .env)'
+      )
       .action(deploy)
-  )
-  .parse(process.argv);
+  ).parse(process.argv);
