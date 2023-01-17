0c5160ec4b8e5d74e9acea9abd4fc297914b4a5f
fakerr-sketch <fm3209742@gmail.com>
2023-01-16 21:53:28 -0300

    code:refactoring

--- cli.js
+++ cli.js
@@ -1,19 +1,15 @@
 import meow from 'meow';
 import notifier from 'update-notifier';
-import deploy from './src/index.js';
+import main from './src/index.js';
 
 var cmd = meow(`
-  Usage: discord_deploy deploy [options]
-
-  Deploy your commands files by searching for "commands" directory in your project. (ATTENTION: Needs "TOKEN", "CLIENT_ID" and "GUILD_ID" variables in .env)
-
+  Usage: discord_deploy deploy [options]\n
   Options:
     -d, --debug  run in debug mode. (default: false)
     --cwd <dir>  Absolute directory to searches for. (default: ${process.cwd()})
     --test       Enables test mode (Requires GUILD_TEST_ID env key). (default: false)
     -h, --help   display CLI Help.
- `,
-  {
+ `,{
     importMeta: import.meta,
     flags: {
       debug: {
@@ -33,17 +29,19 @@ var cmd = meow(`
   }
 );
 
-var { name, version, description } = cmd.pkg;
+var { name, version } = cmd.pkg;
 
 notifier({
   pkg: {
     name,
     version
   },
-  updateCheckInterval: 0
+  updateCheckInterval: 0,
+  shouldNotifyInNpmScript: true
 }).notify({
   message:
-    'New Version is now Available!\n {currentVersion} -> {latestVersion}\n Run `{updateCommand}@latest` to update.'
+    'Version {latestVersion} is now Available!\n Run `{updateCommand}@latest` to update.',
+  defer: false
 });
 
-if (cmd.input.some(stdin => stdin === 'deploy')) await deploy(cmd.flags);
+if (cmd.input.some(stdin => stdin === 'deploy')) await main(cmd.flags);
