aa6ed24f9df4f4597f1e7fdc4e57819549189e0e
fakerr-sketch <fm3209742@gmail.com>
2023-01-16 21:51:51 -0300

    refactor:[add-streams, SR]

--- src/index.js
+++ src/index.js
@@ -5,62 +5,42 @@ import normalize from 'normalize-path';
 import { config } from 'dotenv';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v10';
-import loading from 'ora';
 import utils from './utils.js';
 
-const { _log, __dirname } = utils;
+const { _log, __dirname, spinner } = utils;
 const { applicationGuildCommands } = Routes;
-const spinner = loading('Deploying your commands...');
-const commandsData = new Map();
-commandsData.set('commandsAsJson', []);
-const commandsDataAsJSON = commandsData.get('commandsAsJson');
+const commandsDataAsJSON = [];
 
-function validateCommandObject (file) {
-  Object.keys(file).forEach(fileProp => {
-    if (fileProp === 'data') {
-      commandsDataAsJSON.push(file[fileProp]?.toJSON());
-    }
-  });
-}
-
-async function getCommandFiles (cwd) {
-  var files = await glob('**/commands/**/*.{js,cjs,mjs}', {
-    ignore: ['**/node_modules/**', '**/.git/**'],
-    cwd,
-    absolute: true
-  });
-  if (files.length) {
-    console.time('for-in');
-    forEach(files, async file => {
-      _log('Processing: ' + file);
-      const { default: fileRequired } = await import(
-        normalize(relative(__dirname, file))
-      );
-      validateCommandObject(fileRequired);
-    });
-    console.timeEnd('for-in');
-  } else {
-    _log(
-      '"commands" dir not found. You can change the cwd overriding --cwd option.\n cwd: ' +
-        cwd,
-      'error'
-    );
+async function importCommandFiles (filePath) {
+  var filePathAsString = filePath.toString();
+  _log('Processing: ' + filePathAsString);
+  const { default: fileRequired } = await import(
+    normalize(relative(__dirname, filePathAsString))
+  );
+  if ('data' in fileRequired) {
+    commandsDataAsJSON.push(fileRequired.data.toJSON());
   }
 }
 
-async function deploy ({ cwd, debug, test }) {
-  config({
-    path: resolve(cwd, '.env'),
-    debug
+async function getCommandFiles (options) {
+  var files = glob.stream('**/commands/**/*.{js,cjs,mjs}', {
+    ignore: ['**/node_modules/**', '**/.git/**'],
+    cwd: options.cwd,
+    absolute: true
   });
+  files.on('data', importCommandFiles);
+  files.on('error', error => console.debug(error));
+  files.on('end', () => deploy(options.test));
+}
+
+async function deploy (isTestEnabled) {
   try {
-    await getCommandFiles(cwd);
     var api = new REST({ version: '10' }).setToken(process.env['TOKEN']);
     spinner.start();
     var isCommandsDeployed = await api.put(
       applicationGuildCommands(
         process.env['CLIENT_ID'],
-        test ? process.env['GUILD_TEST_ID'] : process.env['GUILD_ID']
+        isTestEnabled ? process.env['GUILD_TEST_ID'] : process.env['GUILD_ID']
       ),
       {
         body: commandsDataAsJSON
@@ -68,7 +48,7 @@ async function deploy ({ cwd, debug, test }) {
     );
     spinner.stop();
     if (isCommandsDeployed.length) {
-      isCommandsDeployed.forEach(({ name }) =>
+      forEach(isCommandsDeployed, ({ name }) =>
         spinner.succeed('Deployed: ' + name)
       );
       process.exit(0);
@@ -77,6 +57,15 @@ async function deploy ({ cwd, debug, test }) {
     }
   } catch (e) {
     spinner.stop();
+    _log('FATAL: ' + e, 'error');
   }
 }
-export default deploy;
+
+async function main (options) {
+  config({
+    path: resolve(options.cwd, '.env'),
+    debug: options.debug
+  });
+  getCommandFiles(options);
+}
+export default main;
