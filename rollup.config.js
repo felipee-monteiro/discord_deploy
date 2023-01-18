06e3eab9c44e14c5c8471abb5b523bacb1004a1c
fakerr-sketch <fm3209742@gmail.com>
2023-01-17 21:58:44 -0300

    treeshake:add

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,28 @@
 import os from 'node:os';
 import terser from '@rollup/plugin-terser';
 import shebang from 'rollup-plugin-add-shebang';
-
 export default {
   input: './cli.js',
   output: {
     dir: 'bin',
+    minifyInternalExports: true,
     inlineDynamicImports: true
   },
+  treeshake: {
+    preset: 'smallest'
+  },
   external: [
-    'update-notifier',
-    'lodash.foreach',
-    'meow',
-    'ora',
-    'node-fetch',
     'fast-glob',
-    'ansi-styles',
-    'node:url',
-    'dotenv',
-    'normalize-path',
     'node:path'
+    'node:url',
+    'node-fetch',
+    'dotenv',
+    'meow',
+    'ansi-styles',
+    'normalize-path',
+    'lodash.foreach',
+    'ora',
+    'update-notifier',
   ],
-  plugins: [terser({ maxWorkers: os.cpus().length, toplevel: true }), shebang()]
+  plugins: [terser({ maxWorkers: os.cpus().length }), shebang()]
 };
