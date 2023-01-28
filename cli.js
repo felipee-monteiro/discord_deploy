d19950fb416b03c1c25eb8c8adc1fbbd31b7e0e0
fakerr-sketch <fm3209742@gmail.com>
2023-01-28 09:34:16 -0300

    description:resolve

--- cli.js
+++ cli.js
@@ -2,10 +2,10 @@ import meow from 'meow';
 import boxen from 'boxen';
 import notifier from 'update-check';
 import utils from './src/utils.js';
-import main from './src/index.js';
+import { main } from './src/index.js';
 
 const cmd = meow(
-  `\nUsage: discord_deploy deploy [options]\n\nOptions:\n-d, --debug  run in debug mode. (default: false)\n--cwd <dir>  Absolute directory to search for. (default: ${process.cwd()}\n--test Enables test mode (Requires GUILD_TEST_ID env key). (default: false)\n--help   display CLI Help.`,
+  `\nUsage: discord_deploy deploy [options]\n\nOptions:\n-d, --debug  run in debug mode. (default: false)\n--cwd <dir>  Absolute directory to search for. (default: ${process.cwd()})\n--test Enables test mode (Requires GUILD_TEST_ID env key). (default: false)\n--help   display CLI Help.`,
   {
     importMeta: import.meta,
     flags: {
