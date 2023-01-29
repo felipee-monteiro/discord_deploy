5d24b5a972242fad37282a52d2222073b7e1eef0
fakerr-sketch <fm3209742@gmail.com>
2023-01-29 08:16:19 -0300

    help-menu:resolve

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@ import utils from './src/utils.js';
 import { main } from './src/index.js';
 
 const cmd = meow(
-  `\nUsage: discord_deploy deploy [options]\n\nOptions:\n-d, --debug  run in debug mode. (default: false)\n--cwd <dir>  Absolute directory to search for. (default: ${process.cwd()})\n--test Enables test mode (Requires GUILD_TEST_ID env key). (default: false)\n--help   display CLI Help.`,
+  `\nUsage: discord_deploy deploy [options]\n\nOptions:\n--debug  run in debug mode. (default: false)\n--cwd <dir>  Absolute directory to search for. (default: ${process.cwd()})\n--test Enables test mode (Requires GUILD_TEST_ID env key). (default: false)\n--help   display CLI Help.`,
   {
     importMeta: import.meta,
     flags: {
