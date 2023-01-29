df0d17cba2d75353e8fa4facec64854cd3bacf59
fakerr-sketch <fm3209742@gmail.com>
2023-01-29 15:53:33 -0300

    rollup-plugin-esbuild:add

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 import shebang from 'rollup-plugin-add-shebang';
+import esbuild from 'rollup-plugin-esbuild';
 
 export default {
   input: 'cli.js',
   output: {
     dir: 'bin',
-    plugins: [shebang()],
     generatedCode: {
       objectShorthand: true
     }
@@ -27,5 +27,13 @@ export default {
     'lodash.foreach',
     'ora',
     'update-check'
+  ],
+  plugins: [
+    shebang(),
+    esbuild({
+      include: './cli.js',
+      minify: true,
+      target: 'es2020'
+    })
   ]
 };
