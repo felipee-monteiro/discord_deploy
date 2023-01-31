9a18400a97d414cf7f8c90a3d8d802337e646c8c
fakerr-sketch <fm3209742@gmail.com>
2023-01-31 09:48:47 -0300

    typescript:migrate

---
+++ cli.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import { cac } from "cac";
+import { main } from "./src/index.js";
+
+const cli = cac("discord_deploy");
+
+cli.command("deploy").action((argvs) => main(argvs)).option(
+  "debug",
+  "run in debug mode.",
+  {
+    default: false,
+  },
+).option(
+  "cwd",
+  "Absolute directory to search for.",
+  {
+    default: process.cwd(),
+  },
+).option("test", "Enables test mode (Requires GUILD_TEST_ID env key).", {
+  default: false,
+}).option("help", "shows helps menu");
+cli.parse();
