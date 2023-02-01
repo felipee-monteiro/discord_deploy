526bb995470e88f470d624bd5986631de86fffcf
fakerr-sketch <fm3209742@gmail.com>
2023-02-01 17:20:05 -0300

    help-menu:add;version:add

--- cli.ts
+++ cli.ts
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
 import { cac, CAC } from 'cac';
 import { main } from './src';
+import type { Options } from './src/types';
+import { version } from './package.json';
 
 const cli: CAC = cac('discord_deploy');
 
 cli
   .command('deploy')
-  .action(argvs => main(argvs))
+  .action((argvs: Options) => main(argvs))
   .option('debug', 'run in debug mode.', {
     default: false
   })
-  .option('cwd', 'Absolute directory to search for.', {
+  .option('cwd <dir>', 'Absolute directory to search for.', {
     default: process.cwd()
   })
   .option('test', 'Enables test mode (Requires GUILD_TEST_ID env key).', {
     default: false
-  })
-  .option('help', 'shows helps menu');
+  });
+cli.help();
+cli.version(version);
 cli.parse();
