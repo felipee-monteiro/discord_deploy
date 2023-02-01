eea56a186807949bba3b78a1e84896b55f08d24e
fakerr-sketch <fm3209742@gmail.com>
2023-02-01 15:44:52 -0300

    code:format

--- cli.ts
+++ cli.ts
@@ -1,22 +1,20 @@
 #!/usr/bin/env node
-import { cac, CAC } from "cac";
-import { main } from "./src/index.js";
+import { cac, CAC } from 'cac';
+import { main } from './src/index.js';
 
-const cli: CAC = cac("discord_deploy");
+const cli: CAC = cac('discord_deploy');
 
-cli.command("deploy").action((argvs) => main(argvs)).option(
-  "debug",
-  "run in debug mode.",
-  {
-    default: false,
-  },
-).option(
-  "cwd",
-  "Absolute directory to search for.",
-  {
-    default: process.cwd(),
-  },
-).option("test", "Enables test mode (Requires GUILD_TEST_ID env key).", {
-  default: false,
-}).option("help", "shows helps menu");
+cli
+  .command('deploy')
+  .action(argvs => main(argvs))
+  .option('debug', 'run in debug mode.', {
+    default: false
+  })
+  .option('cwd', 'Absolute directory to search for.', {
+    default: process.cwd()
+  })
+  .option('test', 'Enables test mode (Requires GUILD_TEST_ID env key).', {
+    default: false
+  })
+  .option('help', 'shows helps menu');
 cli.parse();
