4f283881e0990a997e964f8d01a5b12bebae98ae
fakerr-sketch <fm3209742@gmail.com>
2023-02-01 15:41:49 -0300

    typescript:add-types

---
+++ src/types.ts
@@ -0,0 +1,36 @@
+import type { SlashCommandBuilder } from 'discord.js';
+
+export type SlashCommand = {
+  readonly name: string;
+  readonly description: string;
+  readonly data?: InstanceType<typeof SlashCommandBuilder>;
+};
+
+export type SlashCommandResponse = {
+  id: string;
+  application_id: string;
+  version: string;
+  default_permission: boolean;
+  default_member_permissions?: null;
+  type: number;
+  nsfw: boolean;
+  name: string;
+  name_localizations?: null;
+  description: string;
+  description_localizations?: null;
+  guild_id: string;
+};
+
+export type FetchDiscordError = {
+  global?: boolean;
+  retry_after?: number;
+  code: number;
+  message: string;
+  errors?: any;
+};
+
+export type Options = Partial<{
+  readonly cwd: string;
+  readonly debug: boolean;
+  readonly test: boolean;
+}>;
