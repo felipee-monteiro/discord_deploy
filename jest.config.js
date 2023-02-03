e217077c28a052b39191ba19e4f4459d21327668
fakerr-sketch <fm3209742@gmail.com>
2023-02-03 09:07:58 -0300

    mjs:to:js

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  transform: {},
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  moduleFileExtensions: ['js', 'ts', 'json', 'node']
+};
