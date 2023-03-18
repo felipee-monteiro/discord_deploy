c34e41d07c96650b0f008419c87e917c75210604
Felipe <fm3209742@gmail.com>
2023-03-18 09:42:41 -0300

    resolves --cwd invalid option

--- cli.ts
+++ cli.ts
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
-import { cac, CAC } from 'cac';
-import { main } from './src';
-import type { Options } from './src/types';
-import { version } from './package.json';
+import { cac, CAC } from "cac";
+import { main } from "./src";
+import type { Options } from "./src/types";
+import { version } from "./package.json";
 
-const cli: CAC = cac('discord_deploy');
+const cli: CAC = cac("discord_deploy");
 
 cli
-  .command('deploy')
+  .command("deploy")
   .action((argvs: Options) => main(argvs))
-  .option('debug', 'run in debug mode.', {
-    default: false
+  .option("debug", "run in debug mode.", {
+    default: false,
   })
-  .option('cwd <dir>', 'Absolute directory to search for.', {
-    default: process.cwd()
+  .option("cwd <dir>", "Absolute directory to search for.", {
+    default: process.cwd(),
+    type: ["string"],
   })
-  .option('test', 'Enables test mode (Requires GUILD_TEST_ID env key).', {
-    default: false
+  .option("test", "Enables test mode (Requires GUILD_TEST_ID env key).", {
+    default: false,
   });
 cli.help();
 cli.version(version);
