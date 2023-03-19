2e3af9f936f4d3b4ee987889240f8bb8796fbcb5
Felipe <fm3209742@gmail.com>
2023-03-19 13:36:02 -0300

    refactoring debugMode

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
-import loading from "ora";
-import colors, { ForegroundColorName } from "ansi-styles";
+import loading from 'ora';
+import colors, { ForegroundColorName } from 'ansi-styles';
 
 interface LogLevels {
   level: string;
@@ -7,7 +7,7 @@ interface LogLevels {
   prefix: string;
 }
 
-function spinner(text: string = "Deploying your files...") {
+function spinner(text: string = 'Deploying your files...') {
   return loading({
     text,
     indent: 1,
@@ -15,34 +15,43 @@ function spinner(text: string = "Deploying your files...") {
 }
 const LOG_LEVELS: LogLevels[] = [
   {
-    level: "info",
-    color: "cyanBright",
-    prefix: "[INFO]",
+    level: 'info',
+    color: 'cyanBright',
+    prefix: '[INFO]',
   },
   {
-    level: "log",
-    color: "greenBright",
-    prefix: "[SUCCESS]",
+    level: 'log',
+    color: 'greenBright',
+    prefix: '[SUCCESS]',
   },
   {
-    level: "warn",
-    color: "yellow",
-    prefix: "[WARNING]",
+    level: 'warn',
+    color: 'yellow',
+    prefix: '[WARNING]',
   },
   {
-    level: "error",
-    color: "redBright",
-    prefix: "[ERROR]",
+    level: 'error',
+    color: 'redBright',
+    prefix: '[ERROR]',
   },
 ];
+
+const debugMode = process.argv.slice(2).filter(arg => arg === '--debug');
 
 export default {
   _log: function (
     message: string,
-    level: "info" | "log" | "warn" | "error" = "info"
+    level: 'info' | 'log' | 'warn' | 'error' = 'info'
   ) {
     const _logProps = LOG_LEVELS.find((log) => log.level === level);
-    if (_logProps) {
+    if (_logProps && debugMode) {
+      console[level](
+        colors[_logProps.color].open,
+        _logProps.prefix,
+        message,
+        colors[_logProps.color].close
+      );
+    } else if (_logProps?.level === 'error') {
       console[level](
         colors[_logProps.color].open,
         _logProps.prefix,
