f120d11dfb6c71127b27133776eaaebad1d3bedd
Felipe <fm3209742@gmail.com>
2023-03-19 14:21:35 -0300

    add clear command

--- cli.ts
+++ cli.ts
@@ -1,24 +1,38 @@
 #!/usr/bin/env node
-import { cac, CAC } from "cac";
-import { main } from "./src";
-import { cwd } from "node:process";
-import { version } from "./package.json";
+import { cac, CAC } from 'cac';
+import { main } from './src';
+import utils from './src/utils';
+import { cwd, exit } from 'node:process';
+import { version } from './package.json';
+import { emptyDir } from 'fs-extra';
 
-const cli: CAC = cac("discord_deploy");
+const cli: CAC = cac('discord_deploy');
 
 cli
-  .command("deploy")
-  .action(main)
-  .option("debug", "run in debug mode.", {
+  .command('deploy')
+  .option('--debug, -d', 'run in debug mode.', {
     default: false,
   })
-  .option("cwd <dir>", "Absolute directory to search for.", {
+  .option('cwd <dir>', 'Absolute directory to search for.', {
     default: cwd(),
-    type: ["string"],
   })
-  .option("test", "Enables test mode.", {
+  .option('--test', 'Enables test mode.', {
     default: false,
-  });
+  })
+  .action(main);
+
+cli.command('clear', 'clear esbuild cache.').action(async () => {
+  try {
+    const isEmpty = await emptyDir('build');
+    // @ts-ignore
+    if (isEmpty.length) utils._log('Cleaned successfully.', 'log');
+    else utils._log('Already Cleaned.');
+    exit(0);
+  } catch (error: any) {
+    utils._log(error.message, 'error');
+    exit(1);
+  }
+});
 cli.help();
 cli.version(version);
 cli.parse();
